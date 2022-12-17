ea300b51c9a370854578290b07a20e86d6263809
Jeremiah Harriott <harriott_47@hotmail.com>
2022-12-17 18:33:21 -0500

    added 4 new files to components and updated index.js file

---
+++ client/src/components/ChannelInner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChannelInner() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ChannelInner
---
+++ client/src/components/TeamMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TeamMessage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default TeamMessage
